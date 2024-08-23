10c06f81c2be604f4b0c69bd758bb15ef49fc89c
mangeshwaghaye <mangeshswaghaye@gmail.com>
2024-08-23 20:45:17 +0530

    test commit

---
+++ java/MyJava.java
@@ -0,0 +1,19 @@
+
+import  java.lang;
+public class MyJava
+{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main (String[] args){
+        MyJava myJava = new MyJava();
+    }
+}
